73ee3c1ce6b638d2c30463aa2c98a8f516542c05
Jordan Molinar-LeBlanc <jordanmolinar-leblanc@Jordans-MacBook-Pro-2.local>
2020-01-02 12:05:31 -0700

    new error updates

--- model/burger.js
+++ model/burger.js
@@ -1,20 +1,21 @@
-const orm = require("../config/orm");
+var orm = require("../config/connection");
 
-let burger = {
-  selectAll: function(cb) {
-    orm.selectAll("burgers", function(res) {
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  insertOne: function(cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function(res) {
-      cb(res);
-    });
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
   },
-  updateOne: function(id, cb) {
+  update: function(id, cb) {
     var condition = "id=" + id;
-    orm.updateOne("burgers", {
+    orm.update("burgers", {
       devoured: true
     }, condition, cb);
   }
